5c7e740dc4f4d205265fa577d0a759becf155df9
Justin Thomas <124629809+JustinCheeramattathil@users.noreply.github.com>
2023-10-25 16:30:17 +0530

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,4 @@
 class Constants {
-  static const int appId = 58323494;
-  static const String appSign = 'b1fb1b607381ec8ec7ec7ccba8f998db51f906d1cc71543637b12b8852b7f619';
+  static const int appId = 0;
+  static const String appSign = '';
 }
